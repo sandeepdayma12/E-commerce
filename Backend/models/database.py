fec985fc539e5362449057635031ae1c9e6339e3
Your Name <sandeepdayma327@gmail.com>
2025-11-06 19:04:00 +0530

    second commit

---
+++ Backend/models/database.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+DATABASE_URL = "postgresql://postgres:your_password@localhost/E-commerce"
+
+
+engine = create_engine(DATABASE_URL)
+
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+Base = declarative_base()
