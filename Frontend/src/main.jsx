15c240883647077516b77af9d21762e1ead963c1
rajen <rajendra.nath@skedgroup.in>
2026-03-17 12:06:24 +0530

    test

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 import App from './App.jsx'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import AppContextProvider from "./context/AppContextProvider";
+import AppContextProvider from "./context/AppContextProvider.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
